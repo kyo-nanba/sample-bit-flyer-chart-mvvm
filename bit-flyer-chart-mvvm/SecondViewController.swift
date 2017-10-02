469f4cb0421cdf3246adec1af683147c8f2a37eb
kyo <kyo.nanba@gmail.com>
2017-10-02 23:55:38 +0900

    Initial Commit

---
+++ bit-flyer-chart-mvvm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bit-flyer-chart-mvvm
+//
+//  Created by kyo on 2017/10/02.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
